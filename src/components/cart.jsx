3200e3797d46fa1e42746d232b7be68b50747c0d
david <david.deguara@mcast.edu.mt>
2021-12-10 20:03:04 +0100

    Updated Cart Item

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -5,6 +5,8 @@ class Cart extends Component {
   render() {
     return (
       <React.Fragment>
+        <h1 className="text-center p-2">Shopping Cart</h1>
+        <CartItem />
         <CartItem />
         <CartItem />
         <CartItem />
--- src/components/cartItem.jsx
+++ src/components/cartItem.jsx
@@ -2,7 +2,24 @@ import React, { Component } from "react";
 
 class CartItem extends Component {
   render() {
-    return <h1>Item</h1>;
+    return (
+      <React.Fragment>
+        <div className="card m-3">
+          <div className="card-header">Item</div>
+          <div className="card-body">
+            <h5 className="card-title">You should totally buy this</h5>
+
+            <p className="card-text">Price: €5</p>
+            <p className="card-text">Quantity: 0</p>
+
+            <div className="btn-group" role="group" aria-label="Quantity">
+              <button className="btn btn-success btn-sm">+</button>
+              <button className="btn btn-danger btn-sm">-</button>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
