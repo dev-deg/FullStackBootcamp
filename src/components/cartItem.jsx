1cbe426cbaeeef157497d4981b649457c65a4b41
david <david.deguara@mcast.edu.mt>
2021-12-17 18:53:25 +0100

    Cart data loading dynamically from state

--- src/components/cartItem.jsx
+++ src/components/cartItem.jsx
@@ -1,16 +1,26 @@
 import React, { Component } from "react";
 
 class CartItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Toothpaste",
+      desc: "Your teeth need this.",
+      price: 3,
+      quantity: 1,
+    };
+  }
+
   render() {
     return (
       <React.Fragment>
         <div className="card m-3">
-          <div className="card-header">Item</div>
+          <div className="card-header">{this.state.name}</div>
           <div className="card-body">
-            <h5 className="card-title">You should totally buy this</h5>
-
-            <p className="card-text">Price: €5</p>
-            <p className="card-text">Quantity: 0</p>
+            <p className="card-text">Description: {this.state.desc}</p>
+            <p className="card-text">Price: €{this.state.price}</p>
+            <p className="card-text">Quantity: {this.state.quantity}</p>
+            <p className="card-text">Total Cost: €</p>
 
             <div className="btn-group" role="group" aria-label="Quantity">
               <button className="btn btn-success btn-sm">+</button>
