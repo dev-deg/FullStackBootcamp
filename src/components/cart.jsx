9d7adc199bf435ce6d09d7c87687b41b3c5a349b
david <david.deguara@mcast.edu.mt>
2021-12-17 20:05:17 +0100

    Added + - quantity

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -6,22 +6,30 @@ class Cart extends Component {
     return (
       <React.Fragment>
         <h1 className="text-center p-2">Shopping Cart</h1>
-        <CartItem name="Floss" desc="Teeth wire" price={2} />
-        <CartItem name="Toothpaste" desc="Goo-like thing" price={3} />
+        <CartItem name="Floss" desc="Teeth wire" price={2} quantity={1} />
+        <CartItem
+          name="Toothpaste"
+          desc="Goo-like thing"
+          price={3}
+          quantity={2}
+        />
         <CartItem
           name="Toothbrush"
           desc="Bamboo thing with a white brush"
           price={4}
+          quantity={1}
         />
         <CartItem
           name="Electric Toothbrush"
           desc="Brushes your teeth automatically"
           price={100}
+          quantity={1}
         />
         <CartItem
           name="Mouthwash"
           desc="Best brand you could buy"
           price={4.5}
+          quantity={1}
         />
       </React.Fragment>
     );
--- src/components/cartItem.jsx
+++ src/components/cartItem.jsx
@@ -7,7 +7,7 @@ class CartItem extends Component {
       name: props.name,
       desc: props.desc,
       price: props.price,
-      quantity: 1,
+      quantity: props.quantity,
     };
   }
 
@@ -20,11 +20,30 @@ class CartItem extends Component {
             <p className="card-text">Description: {this.state.desc}</p>
             <p className="card-text">Price: €{this.state.price}</p>
             <p className="card-text">Quantity: {this.state.quantity}</p>
-            <p className="card-text">Total Cost: €</p>
+            <p className="card-text">
+              Total Cost: €{(this.state.quantity * this.state.price).toFixed(2)}
+            </p>
 
             <div className="btn-group" role="group" aria-label="Quantity">
-              <button className="btn btn-success btn-sm">+</button>
-              <button className="btn btn-danger btn-sm">-</button>
+              <button
+                className="btn btn-success btn-sm"
+                onClick={() =>
+                  this.setState({ quantity: this.state.quantity + 1 })
+                }
+              >
+                +
+              </button>
+              <button
+                className="btn btn-danger btn-sm"
+                onClick={() =>
+                  this.setState({
+                    quantity:
+                      this.state.quantity > 1 ? this.state.quantity - 1 : 1,
+                  })
+                }
+              >
+                -
+              </button>
             </div>
           </div>
         </div>
