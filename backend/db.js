272d1c85982b4d5ece9588aa885d3bce3c0e9e96
david <david.deguara@mcast.edu.mt>
2022-02-25 19:30:48 +0100

    added full CRUD for database and hashing function

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,5 @@
 import { MongoClient } from "mongodb";
+import { createHmac } from "crypto";
 
 const connectionString =
   "mongodb+srv://ecom123456:SSNuhM3vdKkJMCCw@ecom.u9fap.mongodb.net/fsb?retryWrites=true&w=majority";
@@ -18,6 +19,7 @@ export function ConnectDb() {
   }
 }
 
+//Adds a document to collection
 export async function SaveToDb(col, data) {
   try {
     return await db.collection(col).insertOne(data);
@@ -26,12 +28,27 @@ export async function SaveToDb(col, data) {
   }
 }
 
+//Retrieves a document from the collection
 export async function GetFromDb(col, item) {
   try {
     return await db.collection(col).find(item).toArray();
   } catch (error) {
     console.log(error);
   }
+}
+
+//Removes a document from the collection
+export async function RemoveFromDb(col, item) {
+  try {
+    return await db.collection(col).deleteOne(item);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//Used to hash a password
+export function HashString(str) {
+  return createHmac("sha256", str).update("secretPassw0rd").digest("hex");
 }
 
 process.on("exit", () => client.close());
